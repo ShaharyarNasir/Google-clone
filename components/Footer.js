771c875208601dadb8e5772bbc935e74e11d91bc
M Shaharyar Nasir <mshaharyarnasir@gmail.com>
2022-02-08 16:05:24 +0500

    bug fix

--- components/Footer.js
+++ components/Footer.js
@@ -8,7 +8,7 @@ function Footer() {
         </div>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-y-4 grid-flow-row-dense">
             <div className="flex justify-center items-center md:col-span-2 lg:col-span-1 lg:col-start-2">
-               <GlobeIcon className="h-5 mr-1 text-green-700"/> Carbon Neutral since 2007
+               <GlobeIcon className="h-5 mr-1 text-green-700"/> Google Clone by <a href="https://github.com/ShaharyarNasir">Sherry</a>
             </div>
             <div className="flex justify-center space-x-8 whitespace-nowrap md:justify-self-start">
                 <p>Advertising</p>
